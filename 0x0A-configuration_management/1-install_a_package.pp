daffadba87adf0cb31377d8c3ba28ba01f008e86
Kisambira Andrew <andrewkeshaa@gmail.com>
2023-01-12 04:52:09 -0800

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# script to install puppet-lint
+# Using Puppet, install flask from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
